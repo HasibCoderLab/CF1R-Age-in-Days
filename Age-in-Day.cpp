c2a58bccdd46d45443562957642e0f906e8e662a
Hasib Hasan <coderhasibh@gmail.com>
2026-02-19 16:11:19 +0600

    include basic info

---
+++ Age-in-Day.cpp
@@ -0,0 +1,8 @@
+/*============================================================
+    Codeforces - CF1-R
+    Problem : CF1R-Age-in-Days
+    Contest :1 Data Types & Conditions
+    Topic   : Data Types & Conditions
+    File    : CF1R-Age-in-Days.cpp
+    Author  : Hasib Hasan
+============================================================*/
